7402f9848290efe73d2e18a3aaa10655a65ce22a
j23283逵村悠太 <j23283ty@edu.tuis.ac.jp>
2025-05-28 18:40:36 +0900

    Create kadai6-2.py

---
+++ kadai6-2.py
@@ -0,0 +1,28 @@
+##alphavantageからIBMの日別の開始価格、最高値、最安値、終値、取引数を取得し、表示する。
+##一定回数実行するとapi制限がかかる
+
+import requests
+import pandas as pd
+
+APP_ID = "5487O9EK7H412D6N"
+API_URL  = "https://www.alphavantage.co/query"
+
+params = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": "IBM",
+        "apikey": APP_ID,
+        "datatype": "json"
+    }
+
+response = requests.get(API_URL, params=params)
+
+data = response.json()
+data = data['Time Series (Daily)']
+
+df = pd.DataFrame()
+for i in data:
+    df = df._append(pd.Series([data[i]["1. open"],data[i]["2. high"],data[i]["3. low"],data[i]["4. close"],data[i]['5. volume']],name=i))
+
+##省略しないで表示するように
+pd.set_option('display.max_rows', None)
+print(df)
